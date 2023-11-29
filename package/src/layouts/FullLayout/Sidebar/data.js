77038a18000bc23cdecd829dc56a8ab4f960d333
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-11-29 12:51:42 -0800

    Side bar conditional rendering

--- package/src/layouts/FullLayout/Sidebar/data.js
+++ package/src/layouts/FullLayout/Sidebar/data.js
@@ -8,12 +8,13 @@ import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
 // import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
 import AutoAwesomeMosaicOutlinedIcon from '@mui/icons-material/AutoAwesomeMosaicOutlined';
 
-const Menuitems = [
+export const HQMenuitems = [
   {
     title: "Dashboard",
     icon: DashboardOutlinedIcon,
     href: "/dashboards/dashboard1",
  },
+ 
   // {
   //   title: "Autocomplete",
   //   icon: AddToPhotosOutlinedIcon,
@@ -62,4 +63,60 @@ const Menuitems = [
   }
 ];
 
-export default Menuitems;
+export const BRMenuitems = [
+  {
+    title: "Dashboard",
+    icon: DashboardOutlinedIcon,
+    href: "/dashboards/dashboard1",
+ },
+
+  // {
+  //   title: "Autocomplete",
+  //   icon: AddToPhotosOutlinedIcon,
+  //   href: "/form-elements/autocomplete",
+  // },
+  // {
+  //   title: "Buttons",
+  //   icon: AspectRatioOutlinedIcon,
+  //   href: "/form-elements/button",
+  // },
+  // {
+  //   title: "Checkbox",
+  //   icon: AssignmentTurnedInOutlinedIcon,
+  //   href: "/form-elements/checkbox",
+  // },
+  // {
+  //   title: "Radio",
+  //   icon: AlbumOutlinedIcon,
+  //   href: "/form-elements/radio",
+  // },
+  // {
+  //   title: "Slider",
+  //   icon: SwitchCameraOutlinedIcon,
+  //   href: "/form-elements/slider",
+  // },
+  // {
+  //   title: "Switch",
+  //   icon: SwitchLeftOutlinedIcon,
+  //   href: "/form-elements/switch",
+  // },
+  // {
+  //   title: "Form",
+  //   icon: DescriptionOutlinedIcon,
+  //   href: "/form-layouts/form-layouts",
+  // },
+  
+  {
+    title: " Employee List23",
+    icon: AutoAwesomeMosaicOutlinedIcon,
+    href: "/tables/basic-table",
+  },
+  {
+    title: " Monthly Gas Price",
+    icon: AutoAwesomeMosaicOutlinedIcon,
+    href: "/tables/price-table",
+  }
+];
+
+
+
